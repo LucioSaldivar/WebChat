71c0f1fa7dcf3c5a75349c333b615843934b6d33
Lucio Saldivar <43145025+LucioSaldivar@users.noreply.github.com>
2019-04-11 06:10:34 -0700

    Add files via upload
    Another work in progress. This is my way of getting a deeper understanding into sockets.

---
+++ SocketMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace WebChat.SocketsManager
+{
+    public class SocketMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SocketMiddleware(RequestDelegate next, SocketHandler handler)
+        {
+            _next = next;
+            Handler = handler;
+        }
+        private SocketHandler Handler { get; set; }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                return;
+            }
+            var socket = await context.WebSockets.AcceptWebSocketAsync();
+            await Receive(WebSocket socket, Action<WebSocketReceiveResult, byte[]> messageToHandle);
+        }
+
+        private async Task Receive(WebSocket webSocket, Action<WebSocketReceiveResult, byte[]> messageToHandle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
